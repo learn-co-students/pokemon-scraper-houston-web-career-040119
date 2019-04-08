47b42f09dbdabf1fd7df7c7657f8bf193806ffbf
PYN <yxp7531540@gmail.com>
2019-04-08 15:13:21 -0500

    passed

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,34 @@
+require 'pry'
+
 class Pokemon
+
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id:,name:,type:,db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    db.execute('INSERT INTO pokemon (name, type) VALUES (?, ?)',[name,type])
+  end
+
+  def self.find(id, db)
+    data_object = db.execute('SELECT name, type FROM pokemon WHERE id = ?', [id])
+
+    name = data_object.flatten[0]
+    type = data_object.flatten[1]
+
+    Pokemon.new(id: id, name: name, type: type, db: db)
+  end
+
+  def alter_hp(hp, db)
+    db.execute('UPDATE pokemon SET hp = ? WHERE id = ?', [hp, self.id])
+  end
+
+  def hp
+    db.execute('SELECT hp FROM pokemon WHERE id = ?', [self.id]).flatten[0]
+  end
 end
